29f1033924a9262e53287b573cb4dd5d960b4c68
Yaroslav <6715414@gmail.com>
2023-02-02 20:27:15 +0200

    added defaults values and options for CreatableReactSelect

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,17 +1,29 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
+import { v4 as uuidV4 } from "uuid";
 
 import { NoteData, Tag } from "../types";
 
 type NoteFromProps = {
   onSubmit: (data: NoteData) => void;
-};
+  onAddTag: (tag: Tag) => void;
+  avaliableTags: Tag[];
+} & Partial<NoteData>;
 
-export function NoteForm({ onSubmit }: NoteFromProps) {
+export function NoteForm({
+  onSubmit,
+  onAddTag,
+  avaliableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFromProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+  const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
@@ -19,8 +31,9 @@ export function NoteForm({ onSubmit }: NoteFromProps) {
     onSubmit({
       title: titleRef.current!.value,
       markdown: markdownRef.current!.value,
-      tags: [],
+      tags: selectedTags,
     });
+    navigate("..");
   }
 
   return (
@@ -30,14 +43,22 @@ export function NoteForm({ onSubmit }: NoteFromProps) {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required defaultValue={title} />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <CreatableReactSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
                 value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={avaliableTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
                 })}
                 onChange={(tags) => {
@@ -54,7 +75,13 @@ export function NoteForm({ onSubmit }: NoteFromProps) {
         </Row>
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control ref={markdownRef} required as="textarea" rows={15} />
+          <Form.Control
+            ref={markdownRef}
+            defaultValue={markdown}
+            required
+            as="textarea"
+            rows={15}
+          />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
